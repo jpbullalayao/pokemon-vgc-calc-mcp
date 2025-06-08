4a5ffc070a5ed8ef9d55d7a2d9d21c1f5b961feb
Professor Ragna <professorragna@gmail.com>
2025-06-07 19:09:23 -0700

    Remove dist, add gitignore

--- dist/calculator.d.ts
+++
@@ -1,3 +0,0 @@
-import type { CalculateDamageRequest, CalculateDamageResponse } from './types.js';
-export declare function calculateDamage(request: CalculateDamageRequest): CalculateDamageResponse;
-//# sourceMappingURL=calculator.d.ts.map
